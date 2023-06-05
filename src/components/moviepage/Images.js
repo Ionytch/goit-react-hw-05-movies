e59a0aa0ba636a4f125f7133523628fb506270ef
Ionytch <starik.ionytch@gmail.com>
2023-06-05 17:08:22 +0300

    Update Images.js

--- src/components/moviepage/Images.js
+++ src/components/moviepage/Images.js
@@ -1,21 +1,22 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import {  } from "components/api/Api";
 
 
-
-const Images =()=>{
-    const[image, setImage]=useState();
-    const {id} = useParams();
-    console.log (image);
+// const Images =()=>{
+//     const[image, setImage]=useState();
+//     const {id} = useParams();
+//     console.log (image);
     
-    useEffect(() => {
-        if (!id) { return };
-       getImagesByMovieID(id)
-           .then((res) => setImage(res))
-           .catch(error => {
-               console.log('error.message', error.message);
-           });
-   }, [id]);
+//     useEffect(() => {
+//         if (!id) { return };
+//        getImagesByMovieID(id)
+//            .then((res) => setImage(res))
+//            .catch(error => {
+//                console.log('error.message', error.message);
+//            });
+//    }, [id]);
 
-};
+// };
 
-export default Images;
+// export default Images;
